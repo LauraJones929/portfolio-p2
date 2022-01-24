07721c66c127ee770d0922ac250cb37ef8be6653
LauraJones929 <lauraljones19@gmail.com>
2022-01-24 21:04:02 +0000

    feat: Add structure to the script.js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function startGame() {
+
+}
+
+function optionSelected() {
+
+}
+
+function addScore() {
+
+}
+
+function questionOne() {
+
+}
+
+function questionTwo() {
+
+}
+
+function questionThree() {
+
+}
+
+function questionFour() {
+
+}
+
+function questionFive() {
+
+}
+
