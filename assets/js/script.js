08f34b34fd9283f8b2e9ca78849a228581fdc442
LauraJones929 <lauraljones19@gmail.com>
2022-02-02 21:42:39 +0000

    feat: continue to make buttons interactive (if correct, next question appears

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,19 +22,67 @@ let questionData = [
 ];
 
 window.addEventListener("load", function() {
+
+    let currentQuestion = parseInt(localStorage.getItem('location'));
+
     let imageQuestion = document.getElementById("image");
     let button1 = document.getElementById('option-1');
     let button2 = document.getElementById('option-2');
     let button3 = document.getElementById('option-3');
     let button4 = document.getElementById('option-4');
 
-    imageQuestion.src = questionData[0]["image-question"];
-    button1.innerText = questionData[0]["options"][0];
-    button2.innerText = questionData[0]["options"][1];
-    button3.innerText = questionData[0]["options"][2];
-    button4.innerText = questionData[0]["options"][3];
+    imageQuestion.src = questionData[currentQuestion]["image-question"];
+    button1.innerText = questionData[currentQuestion]["options"][0];
+    button2.innerText = questionData[currentQuestion]["options"][1];
+    button3.innerText = questionData[currentQuestion]["options"][2];
+    button4.innerText = questionData[currentQuestion]["options"][3];
 
+    button1.addEventListener("click", function() {
+        if(this.textContent === questionData[currentQuestion]["answer"]) {
+            console.log('You are correct!');
+            this.style.backgroundColor = "green";
+            currentQuestion += 1;
+            localStorage.setItem('location', currentQuestion);
+            window.location.reload();
+        } else {
+            console.log('Try again!');
+            this.style.backgroundColor = "red";
+        }
+    })
+    button2.addEventListener("click", function() {
+        if(this.textContent === questionData[currentQuestion]["answer"]) {
+            console.log('You are correct!');
+            this.style.backgroundColor = "green";
+            currentQuestion += 1;
+            localStorage.setItem('location', currentQuestion);
+            window.location.reload();
+        } else {
+            console.log('Try again!');
+            this.style.backgroundColor = "red";
+        }
+    })
     button3.addEventListener("click", function() {
-        alert("you clicked on "+questionData[0]["options"][2])
+        if(this.textContent === questionData[currentQuestion]["answer"]) {
+            console.log('You are correct!');
+            this.style.backgroundColor = "green";
+            currentQuestion += 1;
+            localStorage.setItem('location', currentQuestion);
+            window.location.reload();
+        } else {
+            console.log('Try again!');
+            this.style.backgroundColor = "red";
+        }
+    })
+    button4.addEventListener("click", function() {
+        if(this.textContent === questionData[currentQuestion]["answer"]) {
+            console.log('You are correct!');
+            this.style.backgroundColor = "green";
+            currentQuestion += 1;
+            localStorage.setItem('location', currentQuestion);
+            window.location.reload();
+        } else {
+            console.log('Try again!');
+            this.style.backgroundColor = "red";
+        }
     })
 })
